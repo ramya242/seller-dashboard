5508d3f979b4339c1696694bc24e9a2460bc548f
Ramya Priya Lakamana <ramya@krify.net>
2020-10-19 10:54:05 +0530

    completed the filter functionality

--- src/app/admin/components/products/product-list/product-list.component.ts
+++ src/app/admin/components/products/product-list/product-list.component.ts
@@ -17,16 +17,59 @@ export class ProductListComponent implements OnInit {
   subSubCategoriesList:any=[]
   availabilitySizes:any=[]
   productAreas:any =[]
+  selectedType:any=1
+  selectedLevel2:any={}
+  selectedLevel3:any={}
+  selectedArea:any={}
+  selectedSize:any={}
+  selectedSortType:any
   constructor(private productService: ProductsService,private adminService: AdminService) { }
+  sortOptions=[
+    {
+      id:"popular",
+      name:"Popularity"
+    },
+    {
+      id:"price_desc",
+      name:"Price -- Low to Hight"
+    },
+    {
+      id:"price_asc",
+      name:"Price -- High to Low"
+    },
+    {
+      id:"newest",
+      name:"Newest First"
+    }
+  ]
   ngOnInit(): void {
     this.getProfileInfo()
     this.categories()
     this.getProductAreas()
     
+    
+  }
+  filterChanged(eve)
+  {
+    console.log(eve)
+    this.selectedSortType = eve
+    this.getAllProductList()
   }
   public getAllProductList()
   {
-      const inputData :any= {"business_userId":this.profileInfo.id,"offset":"0","limit":"20","filters":{"cat_1":[],"cat_2":[],"cat_3":[],"sizes":[],"area":[]},"sorting":"price_asc"};
+      const inputData :any= {
+        "business_userId":this.profileInfo.id,
+        "offset":"0",
+        "limit":"100",
+        "filters":{
+          "cat_1":[this.selectedType],
+          "cat_2":this.selectedLevel2.id?[this.selectedLevel2.id]:[],
+          "cat_3":this.selectedLevel3.id?[this.selectedLevel3.id]:[],
+          "sizes":this.selectedSize.id?[this.selectedSize.id]:[],
+          "area":this.selectedArea.id?[this.selectedArea.id]:[]
+        },
+        "sorting":this.selectedSortType?this.selectedSortType: "price_asc"
+      };
       this.productService.getAllProductList(inputData).subscribe((data: any)=>{
         //console.log('veeru',data)
         if(data.status == 'success')
@@ -41,7 +84,7 @@ export class ProductListComponent implements OnInit {
         if(data.status == 'success')
         {
           this.profileInfo =  data.data
-          this.getAllProductList()
+          this.getSubCategories('level2',this.selectedType)
         }
     })
   }
@@ -76,21 +119,36 @@ export class ProductListComponent implements OnInit {
         if(level=='level1')
         {
           this.categoriesList =  data.data
+          this.subCategoriesList = []
+          this.subSubCategoriesList = []
+          this.availabilitySizes = []
         }
         if(level=='level2')
         {
+          this.selectedType=id
+          this.selectedLevel2 ={}
+          this.selectedLevel3 ={}
+          this.selectedSize={}
+          this.selectedArea={}
           this.subCategoriesList =  data.data
+          this.subSubCategoriesList = []
+          this.availabilitySizes = []
         }
         if(level=='level3')
         {
           this.subSubCategoriesList =  data.data
+          this.availabilitySizes = []
         }
         
       }
+      this.getAllProductList()
      
     }) 
+   
+
   }
   public getAvailabilitySizes(id){
+    this.getAllProductList()
     this.productService.getProductSizes(id).subscribe((data: any)=>{
       if(data.status == 'success')
       {
@@ -107,7 +165,24 @@ export class ProductListComponent implements OnInit {
      
     }) 
   }
-  onCheckboxChange(e) {
+  onCheckboxChange(e,type='',selectedValue="") {
+    if(type=='level2')
+    {
+      this.selectedLevel2 = selectedValue
+    }
+    if(type=='level3')
+    {
+      this.selectedLevel3 = selectedValue
+    }
+    if(type=='size')
+    {
+      this.selectedSize = selectedValue
+    }
+    if(type=='area')
+    {
+      this.selectedArea=selectedValue
+      this.getAllProductList()
+    }
 
   }      
   
